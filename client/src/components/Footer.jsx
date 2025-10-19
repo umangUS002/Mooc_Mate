95f6e982607266ad4bb30ed1ebdfde5237b5e3d2
umangUS002 <btech10271.23@bitmesra.ac.in>
2025-10-19 16:04:40 +0530

    added links

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <div>
-      <footer className="flex border-t-1 border-purple-600 flex-col md:flex-row gap-3 items-center justify-around w-full py-4 text-sm text-white/70">
+      <footer id='footer' className="flex border-t-1 border-purple-600 flex-col md:flex-row gap-3 items-center justify-around w-full py-4 text-sm text-white/70">
         <p>Copyright © 2025 MoocMate. All rights reservered.</p>
         <div className="flex items-center gap-4">
           <a href="#" className="hover:text-white transition-all">
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,30 +1,33 @@
 import React from 'react'
+import {useNavigate} from 'react-router-dom'
 
 function Navbar() {
+    const navigate = useNavigate();
   return (
     <div>
       {/* Navbar */}
                     <nav className="z-50 flex items-center justify-between w-full py-4 px-6 md:px-16 lg:px-24 xl:px-32 backdrop-blur">
-                        <a href='/'>
+                        <a href='/' className='flex gap-2'>
+                            <img className='h-10 max-sm:h-7 hover:scale-105' src="/graduation-cap.svg" alt="" />
                             <h1 className="hover:scale-105 font-bold text-4xl max-sm:text-2xl text-purple-700">MoocMate</h1>
                         </a>
 
-                        <div className="hidden text-white md:flex items-center gap-8 transition duration-500">
-                            <a href="#products" className="hover:text-slate-300 transition">
+                        <div className="cursor-pointer hidden text-white md:flex items-center gap-8 transition duration-500">
+                            <a onClick={()=>navigate('/testPage')} className="hover:text-slate-300 transition">
                                 Practice
                             </a>
                             <a href="#resources" className="hover:text-slate-300 transition">
                                 Resources
                             </a>
-                            <a href="#stories" className="hover:text-slate-300 transition">
-                                Connect Here
+                            <a href="#footer" className="hover:text-slate-300 transition">
+                                Contact Us
                             </a>
                         </div>
 
                         <div className="md:block space-x-3">
                             <button
                                 onClick={() => navigate('/testPage')}
-                                className="cursor-pointer px-6 py-2 z-3 bg-purple-700 hover:bg-purple-950 transition text-white rounded-md"
+                                className="cursor-pointer px-6 max-sm:px-2 max-sm:text-xs py-2 z-3 bg-purple-700 hover:bg-purple-950 transition text-white rounded-md"
                             >
                                 Get started
                             </button>
