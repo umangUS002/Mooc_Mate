8edce765c90841129878d1e335a7a7a38947dbaf
Zaid <161572905+iammdzaidalam@users.noreply.github.com>
2025-10-20 19:57:19 +0530

    fixed .jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import ResultPage from "./components/ResultPage";
-import TestPage from "./pages/TestPage";
+import Home from "./pages/Home.jsx";
+import ResultPage from "./components/ResultPage.jsx";
+import TestPage from "./pages/TestPage.jsx";
 import {Analytics} from '@vercel/analytics/react'
 
 export default function App() {
